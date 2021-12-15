27897c26c62bfd2a41c00e3e8afefedd183ef623
shantanuaggarwal <shantanuaggarwal29@gmail.com>
2021-12-15 13:17:39 -0500

    unsupervised classification

--- example_unsupervised.py
+++ example_unsupervised.py
@@ -28,10 +28,28 @@ from sklearn import linear_model, datasets, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from dbn.models import UnsupervisedDBN # use "from dbn.tensorflow import SupervisedDBNClassification" for computations on TensorFlow
-
+import pandas as pd
 
 ###############################################################################
 # Setting up
+
+
+def prepare_data():
+    train = pd.read_csv("../data/experiments/validation/set_5/train.csv", index_col=False, header=0)
+    test = pd.read_csv("../data/experiments/validation/set_5/test.csv", index_col=False, header=0)
+    cols = ["glcm_contrast_Scaled", "glcm_dissimilarity_Scaled", "glcm_homogeneity_Scaled", "glcm_energy_Scaled",
+            "glcm_correlation_Scaled", "glcm_ASM_Scaled"]
+
+    train_X = train[cols]
+    test_X = test[cols]
+    train_Y = train['Class']
+    test_Y = test['Class']
+
+    # train_X.shape
+    train_feat_new = np.repeat(np.array(train_X), 26, axis=1)
+    test_feat_new = np.repeat(np.array(test_X), 26, axis=1)
+
+    return train_feat_new, test_feat_new, train_Y, test_Y
 
 def nudge_dataset(X, Y):
     """
@@ -64,14 +82,18 @@ def nudge_dataset(X, Y):
     return X, Y
 
 # Load Data
-digits = datasets.load_digits()
-X = np.asarray(digits.data, 'float32')
-X, Y = nudge_dataset(X, digits.target)
-X = (X - np.min(X, 0)) / (np.max(X, 0) + 0.0001)  # 0-1 scaling
+# digits = datasets.load_digits()
+# X = np.asarray(digits.data, 'float32')
+# # X, Y = nudge_dataset(X, digits.target)
+# X, Y =
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
-                                                    test_size=0.2,
-                                                    random_state=0)
+X_train, Y_train, X_test, Y_test = prepare_data()
+X_train = (X_train - np.min(X_train, 0)) / (np.max(X_train, 0) + 0.0001)  # 0-1 scaling
+Y_train = (Y_train - np.min(Y_train, 0)) / (np.max(Y_train, 0) + 0.0001)  # 0-1 scaling
+
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+#                                                     test_size=0.2,
+#                                                     random_state=0)
 
 # Models we will use
 logistic = linear_model.LogisticRegression()
